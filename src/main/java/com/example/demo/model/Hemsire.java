a9027b90b7c4a8dbe1f064d757a5eeb23530c9e0
barisonen <barisonen96@hotmail.com>
2019-07-12 14:50:31 +0300

    The entity "Hemsire" created

---
+++ src/main/java/com/example/demo/model/Hemsire.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Hemsire", uniqueConstraints = @UniqueConstraint(name = "username_uc"
+        , columnNames = "id"))
+public class Hemsire {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
+    @SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
