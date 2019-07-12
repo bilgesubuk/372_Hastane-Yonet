d0534a92569a11fd6c6bfd5590d4c9cd74b346b8
barisonen <barisonen96@hotmail.com>
2019-07-12 13:38:31 +0300

    The entity "Bolum" created

---
+++ src/main/java/com/example/demo/model/Bolum.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Bolum", uniqueConstraints = @UniqueConstraint(name = "username_uc"
+        , columnNames = "isim"))
+public class Bolum {
+
+    @Column(unique = true)
+    private String isim;
+
+    public void setIsim(String isim) { this.isim = isim; }
+    public String getIsim() { return isim; }
+
+}
