c47de681b7db4891c4feb007680fe345da1980a2
barisonen <barisonen96@hotmail.com>
2019-07-12 13:42:58 +0300

    The entity "Ameliyathane" created

---
+++ src/main/java/com/example/demo/model/Ameliyathane.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Ameliyathane", uniqueConstraints = @UniqueConstraint(name = "username_uc"
+        , columnNames = "id"))
+public class Ameliyathane {
+
+    @Column(unique = true)
+    private String id;
+
+    public void setId(String id) { this.id = id; }
+    public String getId() { return id; }
+}
